a6728dc35238b2901e6de48f4e465fb2e73010c5
Алина Можина <mohinalina@gmail.com>
2017-10-12 00:24:03 +0700

    Added a sort of worker threads support

--- task2.rb
+++ task2.rb
@@ -6,32 +6,54 @@
 # - вся логика работы с потоками должны быть вынесена в 
 # отдельный метод, общий для всех итераторов.
 
-class MyArray < Array 
-    def pmap(thr_num, &block)
-        threads = self.each_slice(thr_num)
-            .flat_map { |arr| arr.map { |elem| Thread.new { block.call(elem) } } }
-        threads.map { |thread| thread.value }
-    end  
+require 'thread'
 
-    def pany?(thr_num, &block)
-        arr = pmap(thr_num, &block)
+class Array 
+    def pmap(thr_num, slice_size = 2, &block)
+        queue = Queue.new
+        i = 0
+
+        self.each_slice(slice_size).map { |slice| queue << [i, slice]; i += 1 }
+
+        result = Array.new(i)
+        proc_queue = lambda do
+            until queue.empty?
+                begin
+                    pair = queue.pop(true) 
+                rescue
+                    return                    
+                end             
+
+                result_slice = pair[1].map { |elem| block.call(elem) }
+                result[pair[0]] = result_slice
+            end                
+        end
+
+        threads = (1..thr_num).map { |_| Thread.new(&proc_queue) }
+        threads.map { |thread| thread.join }
+        
+        result.flatten(1)
+    end
+
+    def pany?(thr_num, slice_size = 2, &block)
+        arr = pmap(thr_num, slice_size, &block)
         arr.any?
     end
 
-    def pall?(thr_num, &block)
-        arr = pmap(thr_num, &block)
+    def pall?(thr_num, slice_size = 2, &block)
+        arr = pmap(thr_num, slice_size, &block)
         arr.all?
     end
 
-    def pselect(thr_num, &block)
-        arr = pmap(thr_num) { |elem| [block.call(elem), elem] }
+    def pselect(thr_num, slice_size = 2, &block)
+        arr = pmap(thr_num, slice_size) { |elem| [block.call(elem), elem] }
         filtered_arr = arr.select { |pair| pair[0] }
         filtered_arr.map { |pair| pair[1] }
     end
 end
 
-a = MyArray.new(6) { |i| i + 1 }
-puts (a.pmap(4) { |i| i + 2 } ).inspect
+a = Array.new(6) { |i| i + 1 }
+puts (a.pmap(4, 2) { |i| i + 2 } ).inspect
 
 is_even = lambda { |num| num.even? }
 puts a.pany?(4, &is_even).inspect
