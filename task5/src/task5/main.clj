63b2015bbe4a6b9fbfed99387a6ea8c99b67c835
Алина Можина <mohinalina@gmail.com>
2017-11-10 01:15:59 +0700

    Added memoization

--- task5/src/task5/main.clj
+++ task5/src/task5/main.clj
@@ -1,19 +1,22 @@
 (ns task5.main)
 
+(def trapezium-sum (memoize (fn [func step left right mem-func]
+    (if (> right 0)
+    (+ (mem-func func step left (- right step) mem-func) (func right))
+    0))))
+
 (defn integrate
     [func]
-    (let [trapezium-sum (fn [func step left right]
-          (loop [i (+ left step)
-               result 0]
-            (if (< i right) 
-                (recur (+ i step) (+ result (func i)))
-                result)))
-          trapezium (fn [func step left right] 
+    (let [trapezium (fn [func step left right] 
           (* step (+ (* 0.5 (+ (func left) (func right))) 
-                     (trapezium-sum func step left right))))]
-    (partial trapezium func 0.001 0)))
+                     (trapezium-sum func step left right trapezium-sum))))]
+    (partial trapezium func 0.1 0)))
 
 (defn -main
     [& raw-args]
-    (let [antiderivative (integrate #(Math/sin %))]
-    (println (antiderivative Math/PI))))
+    (let [antiderivative (integrate #(* 2 %))]
+    (time (antiderivative 6))
+    (time (antiderivative 5))
+    (time (antiderivative 4))
+    (time (antiderivative 3))
+    (time (antiderivative 2))))
