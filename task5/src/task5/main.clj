70a903face0ff2a817efec395a98f9c11afe9411
Алина Можина <mohinalina@gmail.com>
2017-11-10 10:39:29 +0700

    Minor task5 refactor

--- task5/src/task5/main.clj
+++ task5/src/task5/main.clj
@@ -1,22 +1,22 @@
 (ns task5.main)
 
-(def trapezium-sum (memoize (fn [func step left right mem-func]
-    (if (> right 0)
-    (+ (mem-func func step left (- right step) mem-func) (func right))
-    0))))
-
 (defn integrate
     [func]
-    (let [trapezium (fn [func step left right] 
-          (* step (+ (* 0.5 (+ (func left) (func right))) 
-                     (trapezium-sum func step left right trapezium-sum))))]
+    (let [trapezium-sum (memoize (fn [func step left right mem-func]
+            (if (> right 0)
+                (+ (mem-func func step left (- right step) mem-func) (func right))
+                0)))
+          trapezium (fn [func step left right]
+            (* step (+ (* 0.5 (+ (func left) (func right))) 
+                       (trapezium-sum func step left right trapezium-sum))))]
     (partial trapezium func 0.1 0)))
 
 (defn -main
     [& raw-args]
     (let [antiderivative (integrate #(* 2 %))]
-    (time (antiderivative 6))
+    (time (antiderivative 0))
     (time (antiderivative 5))
     (time (antiderivative 4))
     (time (antiderivative 3))
-    (time (antiderivative 2))))
+    (time (antiderivative 2))
+    (time (antiderivative 7))))
