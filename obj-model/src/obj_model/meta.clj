a9fbc1c2a3fb3e5a66794baf724e2993cafb60db
Алина Можина <mohinalina@gmail.com>
2018-01-08 04:59:26 +0700

    Added mixins definition prototype

--- obj-model/src/obj_model/meta.clj
+++ obj-model/src/obj_model/meta.clj
@@ -12,10 +12,14 @@
          ::super nil
          ::fields #{}}))))
 
-(defmacro def-class 
+(defmacro def-class
+  "Class definition macro. Adds a map containing class information to class hierarchy."
   [name & sections]
-  (let [s-map (apply assoc 
-                  (cons {} (mapcat identity sections)))
+  {:pre (keyword? name)}
+  (let [s-map (if (empty? sections)
+                {}
+                (apply assoc 
+                  (cons {} (mapcat identity sections))))
         super-class (or (s-map :super) :Object)
         fields (set (s-map :fields))] 
         `(dosync
@@ -28,13 +32,53 @@
                  ::super ~super-class
                  ::fields ~fields}))))))
 
+(defn super-class
+  "Returns parent class of given class."
+  [class]
+  {:pre (keyword? class)}
+  (if (contains? @class-hierarchy class)
+    ((@class-hierarchy class) ::super)
+    (throw (Exception. "Unknown class."))))
+
+; (defn has-field?
+;   "Checks whether class contains field including super classes."
+;   [class field]
+;   {:pre [(keyword? class) (keyword? field)]}
+;   )
+
+(def mixins-methods (ref {}))
+
+ (defmacro enable-method
+   [name mixin]
+   (let [method-desc ((mixins-methods mixin) (keyword name))]
+    `(defn ~name ~(method-desc ::args)
+      ~@(method-desc ::body))))
+
+(defmacro def-mixin-method
+    [name mixin args & body]
+    (let [method-desc {::args args ::body body}
+          curr-mixin-methods (mixins-methods mixin)]
+    (dosync
+      (alter mixins-methods 
+        (fn [mm] 
+          (assoc mm mixin 
+            (assoc curr-mixin-methods (keyword name) method-desc)))))
+    ()))
+
 (defn -main 
-  [& raw-args]  
-  (println @class-hierarchy)
-  (println (macroexpand `(def-class :Animal (:fields (:name)))))
-  (def-class :Animal 
-    (:fields (:name)))
-  (def-class :Cat 
-    (:super :Animal)
-    (:fields (:breed)))
-  (println @class-hierarchy))
+  [& raw-args] 
+  ;(println (macroexpand `(def-mixin-method locate :GPSHolder [obj]
+    ;(println "Hello, position of" obj "!"))))
+  (def-mixin-method locate :GPSHolder [obj]
+    (println "Hello, position of" obj"!")
+    (println "Bye," obj"!"))
+  (enable-method locate :GPSHolder)
+  (locate "Kitty"))
+  ; (println @class-hierarchy)
+  ; (println (macroexpand `(def-class :Animal (:fields (:name)))))
+  ; (def-class :Animal 
+  ;   (:fields (:name)))
+  ; (def-class :Cat 
+  ;   (:super :Animal)
+  ;   (:fields (:breed)))
+  ; (println @class-hierarchy))
